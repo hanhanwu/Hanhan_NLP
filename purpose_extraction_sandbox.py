367ff8cc9002ba7e704882e810074f8a68320dd8
hanhan <wuhanhan999@gmail.com>
2016-06-29 00:01:42 -0700

    update purpose_extraction_sandbox.py

--- purpose_extraction_sandbox.py
+++ purpose_extraction_sandbox.py
@@ -14,7 +14,7 @@ def extract_simple_sequences(s, target_pos_lst):
   i = 0
   pre_idx = -1
   idx_lst = []
-  post_idx = len(pis_lst)
+  post_idx = len(pos_lst)
   while i < target_pos_lst_len:
     idx = get_first_pos_index(target_pos_lst[i], pos_lst, pre_idx, post_idx)
     if idx == -1: return []
@@ -26,14 +26,30 @@ def extract_simple_sequences(s, target_pos_lst):
    
   
 # get whole chunk of tokens with the sprcified start and end
-def get_whole_chunk(s, start_target_pos_lst, end_target_pos_los):
+def get_whole_chunk(s, start_target_pos_lst, end_target_pos_lst):
   token_lst = [tp[0] for tp in s]
   pos_lst = [tp[1] for tp in s]
+  reversed_pos_lst = [pos for pos in reversed(pos_lst)]
+  reversed_end_target_pos_lst = [pos for pos in reversed(end_target_pos_lst)]
   
   i = 0
   j = 0
+  start_idx = -1
+  end_idx = -1
   
+  pre_idx = -1
+  post_idx = len(pos_lst)
+  end_idx_lst = []
   while i < len(end_target_pos_lst):
-    
+    idx = get_first_pos_index(reversed_end_target_pos_lst[i], reversed_pos_lst, pre_idx, post_idx)
+    if idx == -1: return []
+    end_idx_lst.append(len(pos_lst)-1-idx)
+    pre_idx = idx
+    i += 1
+  end_idx_lst = [ei for ei in reserved(end_idx_lst)]
+  
+  post_idx = end_idx_lst[0]
+  while j < len(start_target_pos_lst):
+    idx = get_first_pos_index(start_target_pos_lst[j], pos_lst)
   
   # TO + VB + ..... '.' 
