36fb75c3333181a0ede53664c57f33c96b23da64
hanhan <wuhanhan999@gmail.com>
2016-07-11 23:19:17 -0700

    update what_with_investment _sandbox.py

--- what_with_investment _sandbox.py
+++ what_with_investment _sandbox.py
@@ -1,78 +1,21 @@
-# nearest 'for'...NN.. investment (not purpose/purposes/opportunities)
-def for_NN_investment(ts, s):
+def JJ_NN_investment(ts, s):
   key_start = 'investment'
   key_start_idx = -1
-  followers = ['opportuni', 'purpose', 'propert']
+  filtered_NN = ['future']
   
   if key_start in s:
     for i in range(len(ts)):
       if ts[i][0].startswith(key_start):
         key_start_idx = i
-        break
-        
-  if key_start_idx != -1:
-    for f in followers:
-      if ts[i+1][0].startswith(f):
-        return None
-      
-      flag_ct = 0
-      order = 0
-      pre_idx = key_start_idx - 1
-      combination_start_idx = -1
-      while pre_idx >= 0:
-        if order == 0 and ts[pre_idx][1].startswith('NN'):
-          flag_ct += 1
-          order += 1
-        # nearest 'for'
-        elif order == 1 and ts[pre_idx][1].startswith('for'):
-          combination_start_idx = pre_idx
-          flag_ct += 1
-          order += 1
-        elif order == 2 and (ts[pre_idx][1].startswith('.') or ts[pre_idx][1].startswith('CC') or pre_idx == 0):
-          flag_ct += 1
-          order += 1
-        if flag_ct == 3:
-          return ts[combination_start_idx:key_start_idx+1]
-        
-        pre_idx -= 1
-        
-  return None
-
-
-
-# closest for... investment/investments '.'
-def for_investment_end(ts, s):
-  key_start_idx = -1
-  key_start = 'investment'
   
-  if key_start in s:
-    for i in range(len(ts)):
-      if ts[i][0].startswith(key_start):
-        key_start_idx = i
-        break
-        
   if key_start_idx != -1:
-    flag_ct = 0
-    order = 0
-    pre_idx = key_start_idx - 1
-    combination_start_idx = -1
-    if ts[key_start_idx+1][1].startswith('.') or (len(ts[key_start_idx][0])-len(key_start) >= 2):
-      while pre_idx >= 0:
-        if order == 0 and pre_idx < (key_start_idx - 1) and ts[pre_idx][1].startswith('for'):
-          combination_start_idx = pre_idx
-          order += 1
-          flag_ct +=1
-        elif order == 1 and (ts[pre_idx][1].startswith('.') or ts[pre_idx][1].startswith('CC') or pre_idx == 0):
-            flag_ct += 1
-            order += 1
-        if flag_ct == 2:
-          return ts[combination_start_idx:key_start_idx+1]
-
-        pre_idx -= 1
+    if key_start_idx >= 2 and ts[key_start_idx-1][1].startswith('NN'):
+      if ts[key_start_idx-1][0] not in filtered_NN:
+        if ts[key_start_idx-2][1].startswith('JJ'):
+          return ts[key_start_idx-2 : key_start_idx]
+        else: return ts[key_start_idx-1 : key_start_idx]
       
-  return None
-
-
+      
 
 # investment IN .. NN
 def investment_IN_NN(ts, s):
@@ -84,19 +27,21 @@ def investment_IN_NN(ts, s):
       if ts[i][0].startswith(key_start):
         key_start_idx = i
         break
-  else: return None
-  
-  after_idx = key_start_idx + 1
-  if ts[after_idx][1].startswith('IN'):
-    first_NN = 0
-    end_combination_idx = -1
-    while after_idx < len(ts) and ts[after_idx][1].startswith('.') == False:
-      if ts[after_idx][1].startswith('NN') and first_NN == 0:
-        first_NN += 1
-      elif ts[after_idx][1].startswith('NN') == False and first_NN == 1:
-        end_combination_idx = after_idx
-        return ts[key_start_idx : end_combination_idx]
         
-      after_idx += 1
+  if key_start_idx != -1:
+    after_idx = key_start_idx + 1
+    if ts[after_idx][1].startswith('IN'):
+      first_NN = 0
+      start_combination_idx = after_idx
+      end_combination_idx = -1
+      while after_idx < len(ts) and ts[after_idx][1].startswith('.') == False:
+        if ts[after_idx][1].startswith('NN') and first_NN == 0:
+          first_NN += 1
+        elif ts[after_idx][1].startswith('NN') == False and first_NN == 1:
+          end_combination_idx = after_idx
+          return ts[start_combination_idx+1 : end_combination_idx]
+
+        after_idx += 1
     
   return None
+    
