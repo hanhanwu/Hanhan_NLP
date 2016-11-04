12598d6382a9185ec0da29bbd2942eccafeee9f9
hanhan <wuhanhan999@gmail.com>
2016-11-04 09:05:24 -0700

    update nltk_sentiment_analysis_test.py

--- nltk_sentiment_analysis_test.py
+++ nltk_sentiment_analysis_test.py
@@ -7,7 +7,8 @@ from vaderSentiment import sentiment as vaderSentiment
 sentences = [
               """Most sentiment tools are shit.""",
               """NLTK sentiment analysis is also shit...""",
-              """When the system down, they lost all the money"""
+              """When the system down, they lost all the money""",
+              """I hate Microsoft products! I hate working on Windows! I hate being managed by people!"""
             ]
 for sentence in sentences:
     print(sentence)
