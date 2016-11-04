2f5d08e6e9da1f673693573f4c5da493c1c45c63
hanhan <wuhanhan999@gmail.com>
2016-11-04 09:16:05 -0700

    update nltk_sentiment_analysis_test.py

--- nltk_sentiment_analysis_test.py
+++ nltk_sentiment_analysis_test.py
@@ -8,7 +8,8 @@ sentences = [
               """Most sentiment tools are shit.""",
               """NLTK sentiment analysis is also shit...""",
               """When the system down, they lost all the money""",
-              """I hate Microsoft products! I hate working on Windows! I hate being managed by people!"""
+              """I hate Microsoft products! I hate working on Windows! I hate being managed by people!""",
+              """I hate days when I cannot be happy, and have to work in the office. God, please end this internship soon.... I hate working here. I hate I cannot be happy all the time. Why, today I'm feeling so sad. God, and today I'm so many things to do. I simply want to get more sleep, I'm so tired"""
             ]
 for sentence in sentences:
     print(sentence)
