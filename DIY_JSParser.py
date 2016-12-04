ddd14e3f1f22b10fa706386d6020b451d15fd762
hanhan <wuhanhan999@gmail.com>
2016-12-03 23:53:12 -0800

    create DIY_JSParser.py

---
+++ DIY_JSParser.py
@@ -0,0 +1,22 @@
+# Get Comments and Reactions from new Globe and Mail HTML
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+
+def main():
+    comment_urls = [
+        "http://www.theglobeandmail.com/opinion/a-fascists-win-americas-moral-loss/article32753320/comments/"
+                   ]
+
+    for comment_url in comment_urls:
+        driver = webdriver.Firefox()
+        driver.get(comment_url)
+        time.sleep(5)
+        htmlSource = driver.page_source
+        soup = BeautifulSoup(htmlSource, 'html.parser')
+        authors = [p.find('span').get_text() for p in soup.findAll('p', {'class':'c29cjTJ'})]
+        print(authors)
+
+if __name__ == "__main__":
+    main()
