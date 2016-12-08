bd77d3f4b5c4bdad5bb05344f6e74884b794be10
hanhan <wuhanhan999@gmail.com>
2016-12-07 20:49:07 -0800

    update DIY_JS_Parser2.py

--- DIY_JS_Parser2.py
+++ DIY_JS_Parser2.py
@@ -3,61 +3,77 @@
 # After getting the data, the borwser will be closed.
 from selenium import webdriver
 import time
+import re
+from pprint import pprint
+
+image_reaction_map = {
+                       "B9CMO5aR": 'funny',
+                       "CYVHcC0N":'like',
+                       "0cm0eNFD":'disagree',
+                       "OQlF5Uky":'sad',
+                       "yQIVy5qm":'wow',
+                     }
+
+def get_reaction(img_str):
+    img_ptn = 'https://assets.civilcomments.com/v1/images/reactions/(\w+)_64.png'
+    reaction = image_reaction_map[re.match(img_ptn, img_str).group(1)]
+    return reaction
+
 
 
 def main():
     comment_urls = [
-        "http://www.theglobeandmail.com/opinion/a-fascists-win-americas-moral-loss/article32753320/comments/"
+        "http://www.theglobeandmail.com/opinion/a-fascists-win-americas-moral-loss/article32753320/comments/",
+        "http://www.theglobeandmail.com/opinion/academic-extremism-comes-to-canada/article33185073/comments/"
                    ]
+    article_id_ptn = "http://www.theglobeandmail.com/.*?/article(\d+)/comments"
 
     for comment_url in comment_urls:
+        article_id = re.match(article_id_ptn, comment_url).group(1)
         driver = webdriver.Firefox()
         driver.get(comment_url)
         time.sleep(5)
-        # htmlSource = driver.page_source
 
-        comment_autors = driver.find_elements_by_class_name("c29cjTJ")
-        post_time = driver.find_elements_by_class_name("cNCPihY")
-        comments = driver.find_elements_by_class_name("c1bE414")
+        comment_autors = [ca.text for ca in driver.find_elements_by_class_name("c29cjTJ")]
+        post_time = [pt.text for pt in driver.find_elements_by_class_name("cNCPihY")]
+        comment_text = [cmt.text for cmt in driver.find_elements_by_class_name("c1bE414")]
+        comments_reactions = []
 
-
-        for ca in comment_autors:
-            print(ca.text)
-
-        for pt in post_time:
-            print(pt.text)
-
-        for cmt in comments:
-            print("####", cmt.text, "####")
-        print()
-
-
-        clks= driver.find_elements_by_css_selector('div.c3qHyJD')
-        for clk in clks:
+        clicks= driver.find_elements_by_css_selector('div.c2iexvC')
+        for clk in clicks:
+            driver.execute_script("arguments[0].scrollIntoView();", clk)
             clk.click()
-            print("### click ###")
 
-            reaction_counts = driver.find_elements_by_class_name('c2oytXt')
-            reaction_users = driver.find_elements_by_class_name('c3TwkwL')
-            reaction_time = driver.find_elements_by_class_name('c_0Qpsz')
-            reactions = driver.find_elements_by_class_name('c3S62yx')
+            reaction_counts = [rc.text for rc in driver.find_elements_by_class_name('c2oytXt')]
+            reaction_users = [ru.text for ru in driver.find_elements_by_class_name('c3TwkwL')]
+            reaction_time = [rt.text for rt in driver.find_elements_by_class_name('c_0Qpsz')]
+            reactions = [get_reaction(r.get_attribute('src')) for r in driver.find_elements_by_class_name('c3S62yx')
+                         if 'all' not in r.get_attribute('src')]
+            reaction_lst = []
 
-            for rc in reaction_counts:
-                print(rc.text)
+            for i in range(len(reaction_users)):
+                reaction_lst.append({'reaction_user':reaction_users[i], 'reaction_time':reaction_time[i], 'reaction':reactions[i]})
 
-            for ru in reaction_users:
-                print(ru.text)
+            comments_reactions.append({'reaction_counts':reaction_counts, 'reactions':reaction_lst})
 
-            for rt in reaction_time:
-                print(rt.text)
-
-            for r in reactions:
-                print(r.get_attribute('src'))
-
-            close_clk = driver.find_element_by_css_selector('div.c1KbZcP')
+            close_clk = driver.find_element_by_class_name("c1KbZcP")
             close_clk.click()
 
         driver.close()
+
+        for i in range(len(comment_autors) - len(comments_reactions)):
+            comments_reactions.append({'reaction_counts':0, 'reactions':[]})
+
+        comments = []
+        for idx in range(len(comment_autors)):
+            comments.append({'comment_id':idx, 'comment_author':comment_autors[idx],
+                             'comment_time':post_time[idx], 'comment':comment_text[idx],
+                             'reaction_list':comments_reactions[idx]
+                             })
+        output = {article_id:{'comments':comments}}
+        f_name = r'../../output/article'+article_id+'_comment_reaction.txt'
+        with open(f_name, 'wt') as out:
+            pprint(output, stream=out)
 
 if __name__ == "__main__":
     main()
