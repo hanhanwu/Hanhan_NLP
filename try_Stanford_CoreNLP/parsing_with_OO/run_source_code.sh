658ef6f2a06e15c8e9ed024e38834dfd625ee155
hanhan <wuhanhan999@gmail.com>
2017-03-29 12:35:02 -0700

    update run_source_code.sh

--- try_Stanford_CoreNLP/parsing_with_OO/run_source_code.sh
+++ try_Stanford_CoreNLP/parsing_with_OO/run_source_code.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Preprocess Raw Text Input"
-python3.5 text_preprocessing/preprocess.py  -i '../input/Raw_Text/BOOKS/' -o '../output/Preprocessed_Output/BOOKS/'
-# python3.5 text_preprocessing/preprocess.py  -i '../input/Raw_Text/BOOKS/no1.txt' -o '../output/Preprocessed_Output/BOOKS/'
+# python3.5 text_preprocessing/preprocess.py  -i '../input/Raw_Text/BOOKS/' -o '../output/Preprocessed_Output/BOOKS/'   # used on preprocess.py
+python3.5 text_preprocessing/preprocess2.py  -i '/Users/devadmin/Documents/CF_big_comment_files/CF_big_comment_files' -o '../output/Preprocessed_Output/CF_big_comment_files_test/' -a 'tokenize,ssplit,pos'  # used on preprocess2.py
 echo "Done! Generated Preprocessed Data"
